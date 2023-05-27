af12920496003aac8fc34cd6643c00acfff57acf
raenier <raenierquiambao@gmail.com>
2023-05-27 13:33:37 +0800

    Add a my_each_with_index, yields elements of array and each index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,13 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    self.my_each do |element|
+      yield(element, index)
+      index += 1
+    end
+    return self
+  end
 end
 
 # You will first have to define my_each
